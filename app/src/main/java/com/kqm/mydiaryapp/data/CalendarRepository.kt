1236995b74bc0ea1834bba3a6e600ec58bd310c1
VictorDVLP <crespo.9.11.88@gmail.com>
2024-08-31 21:32:41 +0200

    Complete CalendarRepository with methods quotes data source

--- app/src/main/java/com/kqm/mydiaryapp/data/CalendarRepository.kt
+++ app/src/main/java/com/kqm/mydiaryapp/data/CalendarRepository.kt
@@ -1,10 +1,20 @@
 package com.kqm.mydiaryapp.data
 
+import com.kqm.mydiaryapp.domain.Day
+import com.kqm.mydiaryapp.domain.Quote
 import com.kqm.mydiaryapp.domain.Year
 import com.kqm.mydiaryapp.framework.CalendarDataSourceImpl
+import com.kqm.mydiaryapp.framework.QuoteDataSourceImpl
 
 class CalendarRepository(
-   private val calendarDataSource: CalendarDataSourceImpl
+   private val calendarDataSource: CalendarDataSourceImpl,
+   private val quotesDataSource: QuoteDataSourceImpl
 ) {
     fun getCalendar(): Pair<List<Year>, Int> = calendarDataSource.getRangeDates()
+
+    fun getQuotes() = quotesDataSource.getQuotes()
+
+    suspend fun insertQuote(day: Day) = quotesDataSource.insertQuote(day = day)
+
+    suspend fun deleteQuote(quote: Quote, day: Day) = quotesDataSource.deleteQuote(quote = quote, day = day)
 }
