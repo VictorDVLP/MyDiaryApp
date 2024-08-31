f77a2e34162f9eaf3fad958efeda62147932a6df
VictorDVLP <crespo.9.11.88@gmail.com>
2024-08-31 20:43:06 +0200

    Update functions mappers

--- app/src/main/java/com/kqm/mydiaryapp/framework/Mappers.kt
+++ app/src/main/java/com/kqm/mydiaryapp/framework/Mappers.kt
@@ -1,8 +1,15 @@
 package com.kqm.mydiaryapp.framework
 
+import com.kqm.mydiaryapp.domain.Day
 import com.kqm.mydiaryapp.domain.Quote
 import com.kqm.mydiaryapp.domain.QuoteType
+import com.kqm.mydiaryapp.framework.local.DayWithQuotes
+import com.kqm.mydiaryapp.framework.local.LocalDay
 import com.kqm.mydiaryapp.framework.local.LocalQuote
+
+fun Day.toDayWithQuotes(): DayWithQuotes {
+    return DayWithQuotes(day = this.day.toLocalDay(), quotes = quotes.map { it.toLocalQuote(day = day) })
+}
 
 fun Quote.toLocalQuote(day: Int): LocalQuote {
     return LocalQuote(
@@ -13,6 +20,10 @@ fun Quote.toLocalQuote(day: Int): LocalQuote {
     )
 }
 
+private fun Int.toLocalDay(): LocalDay {
+    return LocalDay(day = this)
+}
+
 private fun QuoteType.toConvertName(): String {
     return when (this) {
         QuoteType.PERSONAL -> "Personal"
@@ -20,6 +31,13 @@ private fun QuoteType.toConvertName(): String {
         QuoteType.FRIEND -> "Amigos"
         QuoteType.WORK -> "Trabajo"
     }
+}
+
+fun DayWithQuotes.toDay(): Day {
+    return Day(
+        day = day.day,
+        quotes = quotes.map { it.toQuote() }
+    )
 }
 
 fun LocalQuote.toQuote(): Quote {
