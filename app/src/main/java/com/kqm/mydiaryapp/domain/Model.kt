a23790b9cbbf11d2c541a4ff67f579de68fcef53
VictorDVLP <crespo.9.11.88@gmail.com>
2024-10-24 12:13:40 +0200

    Adjust Day with isCurrentDay property

--- app/src/main/java/com/kqm/mydiaryapp/domain/Model.kt
+++ app/src/main/java/com/kqm/mydiaryapp/domain/Model.kt
@@ -1,5 +1,8 @@
 package com.kqm.mydiaryapp.domain
 
+import com.kqm.mydiaryapp.framework.parseIdRelation
+import java.time.LocalDate
+
 data class Quote(val hour: String, val note: String, val quoteType: QuoteType)
 
 enum class QuoteType {
@@ -10,7 +13,34 @@ data class Day(
     val day: Int,
     val idRelation: String,
     var quotes: List<Quote> = emptyList()
-)
+) {
+    val isCurrentDay: Boolean
+        get() = isCurrentDay(idRelation)
+
+    private fun isCurrentDay(idRelation: String): Boolean {
+        val (day, month, year) = parseIdRelation(idRelation)
+        val monthNumber = getMonthNumber(month)
+        return LocalDate.now().isEqual(LocalDate.of(year, monthNumber, day))
+    }
+
+    private fun getMonthNumber(monthName: String): Int {
+        return when (monthName) {
+            "Enero" -> 1
+            "Febrero" -> 2
+            "Marzo" -> 3
+            "Abril" -> 4
+            "Mayo" -> 5
+            "Junio" -> 6
+            "Julio" -> 7
+            "Agosto" -> 8
+            "Septiembre" -> 9
+            "Octubre" -> 10
+            "Noviembre" -> 11
+            "Diciembre" -> 12
+            else -> 0
+        }
+    }
+}
 
 data class Month(
     val monthName: String,
