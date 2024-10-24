80aca3ea1f5389e73aee211e8ac4e29350a756b5
VictorDVLP <crespo.9.11.88@gmail.com>
2024-10-24 12:06:30 +0200

    Adapt calendar data flow

--- app/src/main/java/com/kqm/mydiaryapp/usecases/GetDatesUseCase.kt
+++ app/src/main/java/com/kqm/mydiaryapp/usecases/GetDatesUseCase.kt
@@ -1,12 +1,14 @@
 package com.kqm.mydiaryapp.usecases
 
+import androidx.paging.PagingData
 import com.kqm.mydiaryapp.data.CalendarRepository
 import com.kqm.mydiaryapp.domain.Year
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetDatesUseCase @Inject constructor(private val repository: CalendarRepository) {
 
-    operator fun invoke(): Flow<Pair<List<Year>, Int>> = repository.calendarWithQuotes
-
+    operator fun invoke(): Flow<PagingData<Year>> =
+        repository.getCalendarWithQuotes()
 }
